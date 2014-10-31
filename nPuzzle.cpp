b0aa229134ea5ec2470fdffa97556bbffc1395b3
Huy Nguyen <huynt57@gmail.com>
2014-10-31 22:19:12 +0700

    init

---
+++ nPuzzle.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <queue>
+#include <string>
+
+#define FOR(i,a,b) for( int i=(a),_b=(b);i<=_b;i++)
+#define DOW(i,b,a) for( int i=(b),_a=(a);i>=_a;i--)
+
+using namespace std;
+
+typedef pair<int, int> PII;
+typedef unsigned long long ULL;
+typedef long long LL;
+
+int main() {
+	return 0;
+}
+	
