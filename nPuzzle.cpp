dcae4c62e4832939390135d21a4a68bc8c8e7d35
Huy Nguyen <huynt57@gmail.com>
2014-11-09 19:46:28 +0700

    add state, h1 and h2

--- nPuzzle.cpp
+++ nPuzzle.cpp
@@ -33,6 +33,11 @@ int init[maxn][maxn];
 int goal[maxn][maxn];
 int moves;
 
+struct State {
+	int g; //so buoc tu diem bat dau den trang trai
+	int h; // ham danh gia
+	int board[maxn][maxn]; //gia tri cua bang tai mot state
+};
 
 void input() {
 	cin >> size;
@@ -50,17 +55,61 @@ void input() {
 	}
 }
 
+void findNumberInGoalState(int number, int &x, int &y) {
+	FOR(i, 0, size - 1) {
+		FOR(j, 0, size - 1) {
+			if(goal[i][j] == number) {
+				x = i;
+				y = j;
+				break;
+			}
+		}
+	}
+}
 void output()
 {
+	cout << "1";
+}
+//ham danh gia cac o khong o dung vi tri
+int h1(State nextState) {
+	int h;
+	FOR(i, 0, size - 1) {
+		FOR(j, 0, size - 1) {
+			if((nextState.board[i][j] != goal[i][j]) && nextState.board[i][j] != 0) {
+				h++;
+			}
+		}
+	}
+	return h;
 }
 
-int h1() {
+//ham danh gia manhatan
+int h2(State nextState) {
+	int h;
+	int x,y; //toa do se duoc gan cho o trong goal state
+	FOR(i, 0, size - 1) {
+		FOR(j, 0, size - 1) {
+			if(nextState.board[i][j] != 0) {
+				findNumberInGoalState(nextState.board[i][j], x, y);
+				h += abs(x-i) + abs(y-j);
+			}
+		}
+	}
+	return h;	
 }
 
-int h2() {
+int h3(State nextState) {
 }
 
-int h3() {
+int chooseHeuristic(int h, State state) {
+	switch (h) {
+		case 1: 
+			return h1(state);
+		case 2:
+			return h2(state);
+		case 3:
+			return h3(state);
+	}
 }
 
 void solve() {
@@ -68,8 +117,11 @@ void solve() {
 }
 
 int main() {
-	//freopen("nPuzzle.inp", "r", stdin);
-  //freopen("nPuzzle.out", "w", stdout);
+	freopen("nPuzzle.inp", "r", stdin);
+  	freopen("nPuzzle.out", "w", stdout);
+  	input();
+  	output();
+  
 	return 0;
 }
 	
