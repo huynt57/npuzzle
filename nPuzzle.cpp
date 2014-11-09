3e09cc6053d43067174ca794d6b13236d8f66749
Huy Nguyen <huynt57@gmail.com>
2014-11-09 09:55:37 +0700

    read input

--- nPuzzle.cpp
+++ nPuzzle.cpp
@@ -20,14 +20,56 @@
 #define FOR(i,a,b) for( int i=(a),_b=(b);i<=_b;i++)
 #define DOW(i,b,a) for( int i=(b),_a=(a);i>=_a;i--)
 
+#define maxn 100
 using namespace std;
 
 typedef pair<int, int> PII;
 typedef unsigned long long ULL;
 typedef long long LL;
 
+int size;
+int h;
+int init[maxn][maxn];
+int goal[maxn][maxn];
+int moves;
+
+
+void input() {
+	cin >> size;
+	cin >> h;
+	FOR(i, 0, size-1) {
+		FOR(j, 0, size-1) {
+			cin >> init[i][j];
+		}
+	}
+	
+	FOR(i, 0, size-1) {
+		FOR(j, 0, size-1) {
+			cin >> goal[i][j];
+		}
+	}
+}
+
+void output()
+{
+}
+
+int h1() {
+}
+
+int h2() {
+}
+
+int h3() {
+}
+
+void solve() {
+	
+}
 
 int main() {
+	//freopen("nPuzzle.inp", "r", stdin);
+  //freopen("nPuzzle.out", "w", stdout);
 	return 0;
 }
 	
